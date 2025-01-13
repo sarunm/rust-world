c870d9cce435560425ee38ffaaf34f14fc19d464
Sarun M <sarun.maksuanpan@krungsrinimble.com>
2025-01-13 09:20:49 +0700

    re folder

--- iter_and_closure/src/main.rs
+++ iter_and_closure/src/main.rs
@@ -4,4 +4,12 @@ fn main() {
     let result = add(1, 2);
 
     println!("The sum is: {}", result);
+    Lesson();
+}
+
+fn Lesson() {
+    let tressure = vec![100, 200, 300, 400, 500];
+    let double = |x| x * 2;
+    let result: Vec<i32> = tressure.iter().map(double).collect();
+    print!("{:?}", result);
 }
